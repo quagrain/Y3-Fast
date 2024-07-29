3a5af214d48fa28bad4b364a2b8343cbf907ea79
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-07-29 04:50:42 +0000

    Create register_user.js

---
+++ js/register_user.js
@@ -0,0 +1,36 @@
+
+function handleSignUp(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const username = document.getElementById('username').value;
+    const usertype = document.getElementById('usertype').value;
+    const password = document.getElementById('passwd').value;
+    const rePassword = document.getElementById('re_passwd').value;
+
+    const Data = {
+      email: email,
+      username: username,
+      usertype: usertype,
+      passwd1: password,
+      passwd2: rePassword
+    };
+
+    fetch('./actions/register_user_action.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(Data)
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 1) {
+        window.location.href = data.redirect;
+        console.log(response.message)
+      } else {
+        alert('Sign Up failed: ' + data.message);
+      }
+    })
+    .catch(err => console.log('Error:', err));
+}
