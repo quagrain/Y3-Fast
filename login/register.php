7ad7b03b3e5c54f6590da0847af47b6faae02bc2
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-07-30 18:48:41 +0000

    Update register.php

--- login/register.php
+++ login/register.php
@@ -43,7 +43,7 @@
    <?php include '../header.php'?>
 
     <!-- HOME -->
-    <section class="section-hero overlay inner-page bg-image" style="background-image: url('images/hero_1.jpg');" id="home-section">
+    <section class="section-hero overlay inner-page bg-image" style="background-image: url('../images/hero_1.jpg');" id="home-section">
         <div class="container">
             <div class="row">
                 <div class="col-md-7">
