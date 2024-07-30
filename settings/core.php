97feb99095fa56840a0541b03f0ae8213b8ca0c4
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-07-30 17:52:13 +0000

    Update core.php

--- settings/core.php
+++ settings/core.php
@@ -6,7 +6,7 @@ session_start();
 function checkLoggedIn(): void {
     // Check if the user ID session exists
     if (!isset($_SESSION['user_id'])) {
-        header("location: ");
+        header("location: ..login.php");
         die();
     }
 }
