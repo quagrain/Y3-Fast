43adea563f88833c7f346041194aa370d54b36a8
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-07-30 18:40:03 +0000

    Update register.php

--- login/register.php
+++ login/register.php
@@ -5,17 +5,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="css/custom-bs.css">
-    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
-    <link rel="stylesheet" href="css/bootstrap-select.min.css">
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
-    <link rel="stylesheet" href="fonts/line-icons/style.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/animate.min.css">
-    <link rel="stylesheet" href="css/quill.snow.css">
+    <link rel="stylesheet" href="../css/custom-bs.css">
+    <link rel="stylesheet" href="../css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="../css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="../fonts/icomoon/style.css">
+    <link rel="stylesheet" href="../fonts/line-icons/style.css">
+    <link rel="stylesheet" href="../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../css/animate.min.css">
+    <link rel="stylesheet" href="../css/quill.snow.css">
 
     <!-- MAIN CSS -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body id="top">
 
@@ -40,7 +40,7 @@
 
 
     <!-- NAVBAR -->
-   <?php include 'header.php'?>
+   <?php include '../header.php'?>
 
     <!-- HOME -->
     <section class="section-hero overlay inner-page bg-image" style="background-image: url('images/hero_1.jpg');" id="home-section">
@@ -49,7 +49,7 @@
                 <div class="col-md-7">
                     <h1 class="text-white font-weight-bold">SignUp</h1>
                     <div class="custom-breadcrumbs">
-                        <a href="index.php">Home</a> <span class="mx-2 slash">/</span>
+                        <a href="../index.php">Home</a> <span class="mx-2 slash">/</span>
                         <span class="text-white"><strong>Sign Up</strong></span>
                     </div>
                 </div>
@@ -63,7 +63,7 @@
                 <div class="col-lg-6 mb-5">
                     <h2 class="mb-4">Sign Up To JobBoard</h2>
 
-                    <form action="actions/register_user_action.php" class="p-4 border rounded shadow" novalidate>
+                    <form action="" class="p-4 border rounded shadow" novalidate>
 
                         <div class="row form-group">
                             <div class="col-md-12 mb-3 mb-md-0">
@@ -121,12 +121,12 @@
 
                         <div class="row form-group">
                             <div class="col-md-12">
-                                <input type="submit" value="Sign Up" class="btn px-4 btn-primary text-white" id="submitBtn">
+                                <input type="submit" value="Sign Up" class="btn px-4 btn-primary text-white" id="submitBtn" onclick="handleSignUp(event)">
                             </div>
                         </div>
 
                         <div class="row justify-content-end pr-3">
-                            <a href="login.php" class="font-weight-light text-primary small">Have an account? Log in!</a>
+                            <a href="../login/login.php" class="font-weight-light text-primary small">Have an account? Log in!</a>
                         </div>
                     </form>
                     
@@ -135,24 +135,26 @@
         </div>
     </section>
 
-    <?php include 'footer.php'?>
+    <?php include '../footer.php'?>
 
 </div>
 
 <!-- SCRIPTS -->
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="js/isotope.pkgd.min.js"></script>
-<script src="js/stickyfill.min.js"></script>
-<script src="js/jquery.fancybox.min.js"></script>
-<script src="js/jquery.easing.1.3.js"></script>
-<script src="js/jquery.waypoints.min.js"></script>
-<script src="js/jquery.animateNumber.min.js"></script>
-<script src="js/owl.carousel.min.js"></script>
-<script src="js/quill.min.js"></script>
-<script src="js/bootstrap-select.min.js"></script>
-<script src="js/custom.js"></script>
-<script src="js/validation.js"></script>
+<script src="../js/jquery.min.js"></script>
+<script src="../js/bootstrap.bundle.min.js"></script>
+<script src="../js/isotope.pkgd.min.js"></script>
+<script src="../js/stickyfill.min.js"></script>
+<script src="../js/jquery.fancybox.min.js"></script>
+<script src="../js/jquery.easing.1.3.js"></script>
+<script src="../js/jquery.waypoints.min.js"></script>
+<script src="../js/jquery.animateNumber.min.js"></script>
+<script src="../js/owl.carousel.min.js"></script>
+<script src="../js/quill.min.js"></script>
+<script src="../js/bootstrap-select.min.js"></script>
+<script src="../js/custom.js"></script>
+<script src="../js/validation.js"></script>
+
+<script src="../js/register_user.js"></script>
 
 </body>
 </html>
