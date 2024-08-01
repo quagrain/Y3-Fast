129c45d162b2d05975b634dd7d4f1e8bdaaa28f6
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-08-01 01:17:57 +0000

    Update register_user.js

--- js/register_user.js
+++ js/register_user.js
@@ -8,15 +8,40 @@ function handleSignUp(event) {
     const password = document.getElementById('passwd').value;
     const rePassword = document.getElementById('re_passwd').value;
 
+    var fname, lname, dob, occup, descrip;
+    var org_name, creation_date, industry, tagIds;
+
+    if (usertype=='JobSeeker') {
+        fname = document.getElementById('fname').value;
+        lname = document.getElementById('lname').value;
+        dob = document.getElementById('date_of_birth').value;
+        occup = document.getElementById('occupation').value;
+        descrip = document.getElementById('description').value;
+    } else if (usertype=='Employer') {
+        org_name = document.getElementById('org_name').value;
+        creation_date = document.getElementById('creation_date').value;
+        industry = document.getElementById('industry').value;
+        // tagIds = document.getElementById('occupation').value;
+    }
+
     const Data = {
       email: email,
       username: username,
       usertype: usertype,
       passwd1: password,
-      passwd2: rePassword
+      passwd2: rePassword,
+      fname: fname,
+      lname: lname,
+      dob: dob,
+      occup: occup,
+      descrip: occup,
+      org_name: org_name,
+      creation_date: creation_date,
+      industry: industry,
+      tagIds: {}
     };
 
-    fetch('./actions/register_user_action.php', {
+    fetch('../actions/register_user_action.php', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
