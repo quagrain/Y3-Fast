7e3304b66f6280b042cd70f8d8a62bf54ffebd03
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-08-01 20:56:50 +0000

    Update jobBoardStats.php

--- functions/jobBoardStats.php
+++ functions/jobBoardStats.php
@@ -2,10 +2,46 @@
 
 include '../settings/connection.php';
 
-function getNumCandidates(){}
+function getNumCandidates(){
+    global $conn;
+    $query = "SELECT COUNT(user_id) AS num_count FROM job_seekers";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    $numCandidates = $row['num_count'];
+    return $numCandidates;
+}
 
-function getNumJobsPosted(){}
+function getNumJobsPosted(){
+    global $conn;
+    $query = "SELECT COUNT(job_id) AS num_count FROM job_req";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    $numJobsPosted = $row['num_count'];
+    return $numJobsPosted;
+}
 
-function getNumJobsFilled(){}
+function getNumJobsFilled(){
+    global $conn;
+    $query = "SELECT COUNT(app_id) AS num_count FROM applications";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    $numJobsFilled = $row['num_count'];
+    return $numJobsFilled;
+}
 
-function getNumCompanies(){}
+function getNumCompanies(){
+    global $conn;
+    $query = "SELECT COUNT(org_name) AS num_count FROM employers";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    $numCompanies = $row['num_count'];
+    return $numCompanies;
+}
