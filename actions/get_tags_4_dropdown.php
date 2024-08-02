e425bd4759001e2a3b6dd0532ebaff2d90bacb96
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-08-02 02:13:55 +0000

    Update get_tags_4_dropdown.php

--- actions/get_tags_4_dropdown.php
+++ actions/get_tags_4_dropdown.php
@@ -4,11 +4,11 @@ include './functions/getAllTags.php';
 
 $tags = getAllTags();
 
-if ($tags = false) {
-    $response = ["status" => 0, "message" => "failed", "result" => null];
-} else {
-    $response = ["status" => 1, "message" => "successful", "result" => $tags];
-}
+// if ($tags = false) {
+//     $response = ["status" => 0, "message" => "failed", "result" => null];
+// } else {
+//     $response = ["status" => 1, "message" => "successful", "result" => $tags];
+// }
 
-echo json_encode($response);
-exit();
+// echo json_encode($response);
+// exit();
