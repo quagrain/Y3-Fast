e32f94b3af893a0c0c69efe13a6322709d68106b
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-08-04 04:12:42 +0000

    Update index.php

--- index.php
+++ index.php
@@ -235,44 +235,6 @@
                         </div>
                     </div>
 
-                    <ul class="job-listings mb-5">
-                        <li
-                            class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center"
-                        >
-                            <a href="job-single.html"></a>
-                            <div class="job-listing-logo">
-                                <img
-                                    src="images/job_logo_1.jpg"
-                                    alt="Free Website Template by Free-Template.co"
-                                    class="img-fluid"
-                                />
-                            </div>
-
-                            <div
-                                class="job-listing-about d-sm-flex custom-width w-100 justify-content-between mx-4"
-                            >
-                                <div
-                                    class="job-listing-position custom-width w-50 mb-3 mb-sm-0"
-                                >
-                                    <h2>Product Designer</h2>
-                                    <strong>Adidas</strong>
-                                </div>
-                                <div
-                                    class="job-listing-location mb-3 mb-sm-0 custom-width w-25"
-                                >
-                                    <span class="icon-room"></span> New York,
-                                    New York
-                                </div>
-                                <div class="job-listing-meta">
-                                    <span class="badge badge-danger"
-                                        >Part Time</span
-                                    >
-                                </div>
-                            </div>
-                        </li>
-                        
-                    </ul>
-
                     <?php
                         $totalJobs = getNumJobsPosted();
                         $jobsPerPage = 7;
@@ -281,6 +243,12 @@
                         $start = ($currentPage - 1) * $jobsPerPage + 1;
                         $end = min($start + $jobsPerPage - 1, $totalJobs);
                     ?>
+
+                    <?php 
+                        include 'functions/getJobListings.php';
+                        getJobListings($start, $jobsPerPage);
+                    ?>
+
                     <div class="row pagination-wrap">
                         <div class="col-md-6 text-center text-md-left mb-4 mb-md-0">
                             <span>Showing <?= $start ?>-<?= $end ?> Of <?= $totalJobs ?></span>
