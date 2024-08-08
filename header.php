c778231284ce341ea9a48d43d2936c3a9a52d6d7
Victor  Quagraine <Victorquagraine9@gmail.com>
2024-08-08 04:32:14 +0000

    Logout css

--- header.php
+++ header.php
@@ -29,31 +29,42 @@
                 </ul>
             </nav>
 
-            <div class="right-cta-menu text-right d-flex align-items-center col-6">
-                <div class="ml-auto">
-                    <?php
-                    if (isset($_SESSION['user_id'])) { // If the user is logged in display these buttons
-                        if (isset($_SESSION['role']) && $_SESSION['role'] == "Employer") { // if the current user is an employer show the post job button
-                            echo '
+            <div class="right-cta-menu text-right d-flex align-items-center col-3 pr-5">
+                <?php
+                if (isset($_SESSION['user_id'])) { // If the user is logged in display these buttons
+                    if (isset($_SESSION['role']) && $_SESSION['role'] == "Employer") { // if the current user is an employer show the post job button
+                        echo '
+                            <div class="ml-2 pl-pr-5">
                                 <a href="post-job.php" class="btn btn-outline-white border-width-2 d-none d-lg-inline-block">
-                                <span class="mr-2 icon-add"></span>Post a Job</a>';
-                        }
-                        echo '<a href="profile.php" class="btn btn-primary border-width-2 d-none d-lg-inline-block">
-                                <span class="mr-2 icon-user-o"></span>Profile</a>';
-                        echo '<a href="./actions/logout.php" class="btn btn-primary border-width-2 d-none d-lg-inline-block">
-                                <span class="mr-2 icon-user-o"></span>Logout</a>';
-                    } else {
-                        $current_page = basename($_SERVER['PHP_SELF']);
-                        if ($current_page == 'login.php') {
-                            echo '<a href="register.php" class="btn btn-primary border-width-2 d-none d-lg-inline-block">
-                                    <span class="mr-2 icon-user-plus"></span>Sign Up</a>';
-                        } else {
-                            echo '<a href="login.php" class="btn btn-primary border-width-2 d-none d-lg-inline-block">
-                                    <span class="mr-2 icon-lock_outline"></span>Login</a>';
-                        }
+                                <span class="icon-add"></span>Add Job</a>
+                            </div>';
                     }
-                    ?>
-                </div>
+                    echo '
+                              <div class="ml-2">
+                              <a href="profile.php" class="btn btn-primary border-width-2 d-none d-lg-inline-block">
+                                <span class="mr-2 icon-user-o"></span>Profile</a>
+                              </div>';
+                    echo '<div class="ml-2">
+                              <a href="./actions/logout.php" class="btn btn-outline-white border-width-2 d-none d-lg-inline-block">
+                                <span class="icon-exit_to_app"></span></a>
+                              </div>';
+                } else {
+                    $current_page = basename($_SERVER['PHP_SELF']);
+                    if ($current_page == 'login.php') {
+                        echo '
+                            <div class="ml-2">
+                            <a href="register.php" class="btn btn-primary border-width-2 d-none d-lg-inline-block">
+                                    <span class="mr-4 icon-user-plus"></span>Sign Up</a>
+                            </div>';
+                    } else {
+                        echo '
+                            <div class="ml-2">
+                                <a href="login.php" class="btn btn-primary border-width-2 d-none d-lg-inline-block">
+                                    <span class="mr-4 icon-lock_outline"></span>Login</a>
+                            </div>';
+                    }
+                }
+                ?>
                 <a href="#"
                    class="site-menu-toggle js-menu-toggle d-inline-block d-xl-none mt-lg-2 ml-3"
                 ><span class="icon-menu h3 m-0 p-0 mt-2"></span>
