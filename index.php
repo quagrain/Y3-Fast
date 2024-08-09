161acf344c8f24d13a49806526f82e55a1737cb0
Victor  Quagraine <Victorquagraine9@gmail.com>
2024-08-09 13:58:36 +0000

    index fix

--- index.php
+++ index.php
@@ -94,14 +94,14 @@ include './functions/jobBoardStats.php';
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-md-12 popular-keywords">
-                                    <h3>Trending Keywords:</h3>
-                                    <ul class="keywords list-unstyled m-0 p-0">
-                                        <li><a href="#" class="">UI Designer</a></li>
-                                        <li><a href="#" class="">Python</a></li>
-                                        <li><a href="#" class="">Developer</a></li>
-                                    </ul>
-                                </div>
+                                <!--                                <div class="col-md-12 popular-keywords">-->
+                                <!--                                    <h3>Trending Keywords:</h3>-->
+                                <!--                                    <ul class="keywords list-unstyled m-0 p-0">-->
+                                <!--                                        <li><a href="#" class="">UI Designer</a></li>-->
+                                <!--                                        <li><a href="#" class="">Python</a></li>-->
+                                <!--                                        <li><a href="#" class="">Developer</a></li>-->
+                                <!--                                    </ul>-->
+                                <!--                                </div>-->
                             </div>
                         </form>
 
